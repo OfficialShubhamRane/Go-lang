d3e6ae367b03fc35bf479b82023cb531abb682dd
Shubham Rane <official.shubhamrane.com>
2024-08-22 18:24:05 -0500

    almost done

--- react-go-tutorial/client/src/main.tsx
+++ react-go-tutorial/client/src/main.tsx
@@ -1,14 +1,15 @@
-import React, { StrictMode } from 'react'
+import  { StrictMode } from 'react'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ReactDOM from 'react-dom/client'
+import theme from './chakra/theme.ts'
 
-const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={new QueryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </QueryClientProvider>
